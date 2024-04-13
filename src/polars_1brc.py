e4eb68d37c7c0e13a5bcd999eba66168a1d107aa
Doug Mercer <dougmercer@users.noreply.github.com>
2024-04-13 15:49:00 -0400

    Include the right polars implementation

--- src/polars_1brc.py
+++ src/polars_1brc.py
@@ -1,26 +1,30 @@
 import polars as pl
 
+pl.Config.set_streaming_chunk_size(8_000_000)
 
-def create_polars_df(filename):
-    pl.Config.set_streaming_chunk_size(8_000_000)
-    return (
-        pl.scan_csv(
-            filename,
-            separator=";",
-            has_header=False,
-            new_columns=["station", "measure"],
-        )
-        .group_by(by="station")
-        .agg(
-            max=pl.col("measure").max(),
-            min=pl.col("measure").min(),
-            mean=pl.col("measure").mean(),
-        )
-        .sort("station")
-        .collect(streaming=True)
+df = (
+    pl.scan_csv(
+        "data/measurements.txt",
+        separator=";",
+        has_header=False,
+        new_columns=["city", "value"],
     )
+    .group_by("city")
+    .agg(
+        pl.min("value").alias("min"),
+        pl.mean("value").alias("mean"),
+        pl.max("value").alias("max"),
+    )
+    .sort("city")
+    .collect(streaming=True)
+)
 
-
-if __name__ == "__main__":
-    df = create_polars_df("data/measurements.txt")
-    print(df)
+print(
+    "{",
+    ", ".join(
+        f"{data[0]}={data[1]:.1f}/{data[2]:.1f}/{data[3]:.1f}"
+        for data in df.iter_rows()
+    ),
+    "}",
+    sep="",
+)
