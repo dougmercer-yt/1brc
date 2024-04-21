b1ccfb500571e96e261989b4dd05b8b48796cb37
Doug Mercer <dougmercer@users.noreply.github.com>
2024-04-21 18:38:00 -0400

    Add wouterlvv's try/except trick.

--- eval.sh
+++ eval.sh
@@ -6,6 +6,7 @@ commands=(
     "python src/duckdb_1brc.py"
     "python src/polars_1brc.py"
     "pypy3 src/community/farquhar_v6.py --workers 10 --chunk_size 536870912"
+    "pypy3 src/community/doug_booty4_wouter.py"
 )
 
 for cmd in "${commands[@]}"; do
